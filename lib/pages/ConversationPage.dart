b83377df54eafca7fd21b78c0909a5cf1837151a
Daniel Lopez <dani.lopez@gmail.com>
2019-08-21 12:47:29 +0200

    Añadido slideable widget

--- lib/pages/ConversationPage.dart
+++ lib/pages/ConversationPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:kidsmessaging/config/Palette.dart';
+import 'package:kidsmessaging/pages/ConversationBottomSheet.dart';
 import 'package:kidsmessaging/widgets/ChatAppBar.dart';
 import 'package:kidsmessaging/widgets/ChatListWidget.dart';
 import 'package:kidsmessaging/widgets/InputWidget.dart';
@@ -12,22 +14,32 @@ class ConversationPage extends StatefulWidget {
 
 class _ConversationPageState extends State<ConversationPage> {
 
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
             appBar: ChatAppBar(), // Custom app bar for chat screen
-            body: Stack(children: <Widget>[
+            body: Container(
+              color: Palette.chatBackgroundColor,
+              child:Stack(children:<Widget>[
               Column(
                 children: <Widget>[
                   ChatListWidget(),//Chat list
-                  InputWidget() // The input widget
+                  GestureDetector(
+                    child: InputWidget(),
+                    onPanUpdate: (details){
+                      if (details.delta.dy <0){
+                        _scaffoldKey.currentState
+                            .showBottomSheet<Null>((BuildContext context) {
+                              return ConversationBottomSheet();
+                        });
+                      }
+                    })
                 ],
               ),
-            ]
-            )
-        )
-    );
+            ]))));
   }
 
 
